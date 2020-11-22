19245251157bc359776ff0c0d1a43c5a93face55
Hemant Bundele <Hemant.Bundele@gmail.com>
2020-11-22 11:57:41 -0500

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/iblaunch/wpassverifier/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.iblaunch.wpassverifier.domain;
---
+++ src/main/java/com/iblaunch/wpassverifier/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.iblaunch.wpassverifier.web.rest.vm;
